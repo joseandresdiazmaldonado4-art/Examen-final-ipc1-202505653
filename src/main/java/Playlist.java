b3b71c8ec01223e39ad91c9849e369a591d33009
Jose <joseandresdiazmaldonado4@gmail.com>
2026-05-02 11:56:49 -0600

    Commit 3

--- src/main/java/Playlist.java
+++ src/main/java/Playlist.java
@@ -14,6 +14,38 @@ public class Playlist {
         this.tamanio = 0;
     }
 
+    public NodoDoble getCabeza() {
+        return cabeza;
+    }
+
+    public void setCabeza(NodoDoble cabeza) {
+        this.cabeza = cabeza;
+    }
+
+    public NodoDoble getCola() {
+        return cola;
+    }
+
+    public void setCola(NodoDoble cola) {
+        this.cola = cola;
+    }
+
+    public NodoDoble getActual() {
+        return actual;
+    }
+
+    public void setActual(NodoDoble actual) {
+        this.actual = actual;
+    }
+
+    public int getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(int tamanio) {
+        this.tamanio = tamanio;
+    }
+
     public void agregarCancion(Cancion c) {
         NodoDoble nuevo = new NodoDoble(c);
 
@@ -28,6 +60,69 @@ public class Playlist {
         }
 
         tamanio++;
+    }
+
+    public Cancion siguiente() {
+        if (actual == null) {
+            return null;
+        }
+
+        if (actual.getSiguiente() == null) {
+            return null;
+        }
+
+        actual = actual.getSiguiente();
+        actual.getDato().reproducir();
+        return actual.getDato();
+    }
+
+    public Cancion anterior() {
+        if (actual == null) {
+            return null;
+        }
+
+        if (actual.getAnterior() == null) {
+            return null;
+        }
+
+        actual = actual.getAnterior();
+        actual.getDato().reproducir();
+        return actual.getDato();
+    }
+
+    public void eliminarActual() {
+        if (actual == null) {
+            System.out.println("No hay cancion actual para eliminar.");
+            return;
+        }
+
+        NodoDoble nodoEliminar = actual;
+
+        if (cabeza == cola) {
+            cabeza = null;
+            cola = null;
+            actual = null;
+        } else if (nodoEliminar == cabeza) {
+            cabeza = cabeza.getSiguiente();
+            cabeza.setAnterior(null);
+            actual = cabeza;
+        } else if (nodoEliminar == cola) {
+            cola = cola.getAnterior();
+            cola.setSiguiente(null);
+            actual = cola;
+        } else {
+            NodoDoble anteriorNodo = nodoEliminar.getAnterior();
+            NodoDoble siguienteNodo = nodoEliminar.getSiguiente();
+
+            anteriorNodo.setSiguiente(siguienteNodo);
+            siguienteNodo.setAnterior(anteriorNodo);
+
+            actual = siguienteNodo;
+        }
+
+        tamanio--;
+
+        System.out.println("Cancion eliminada: " + nodoEliminar.getDato().getTitulo());
     }
 
     public void mostrarPlaylist() {
--- src/main/java/com/mycompany/proyecto1/labfinal/Labfinal.java
+++ src/main/java/com/mycompany/proyecto1/labfinal/Labfinal.java
@@ -4,7 +4,7 @@ public class Labfinal {
 
     public static void main(String[] args) {
 
-        Cancion cancion1 = new Cancion("Enseñame a Bailar", 212, "Bad Bunny");
+        Cancion cancion1 = new Cancion("Enseniame a Bailar", 212, "Bad Bunny");
         Cancion cancion2 = new Cancion("Perderme Contigo", 205, "Bacilos");
         Cancion cancion3 = new Cancion("Die For You", 260, "The Weeknd");
 
@@ -13,6 +13,20 @@ public class Labfinal {
         playlist.agregarCancion(cancion1);
         playlist.agregarCancion(cancion2);
         playlist.agregarCancion(cancion3);
+
+        playlist.mostrarPlaylist();
+
+        System.out.println("----- SIGUIENTE -----");
+        playlist.siguiente();
+
+        System.out.println("----- SIGUIENTE -----");
+        playlist.siguiente();
+
+        System.out.println("----- ANTERIOR -----");
+        playlist.anterior();
+
+        System.out.println("----- ELIMINAR ACTUAL -----");
+        playlist.eliminarActual();
 
         playlist.mostrarPlaylist();
     }
